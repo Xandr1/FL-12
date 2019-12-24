aa001cfb4baa916aba5eeb51774d73081059261f
Oleksandr Kirsanov <olek.kirsanov@gmail.com>
2019-12-24 23:00:00 +0200

    Update

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,26 +1,21 @@
-const a = parseInt(prompt("input length of side a:"));
-const b = parseInt(prompt("input length of side b:"));
-const c = parseInt(prompt("input length of side c:"));
+const a = parseInt(prompt('Input length of side a:'));
+const b = parseInt(prompt('Input length of side b:'));
+const c = parseInt(prompt('Input length of side c:'));
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-    alert("Input values should be obly numbers");
-} 
-  else if (a <= 0 || b <= 0 || c <= 0) {
-      console.log("Triangle doesn\'t exist");
-      alert("A triangle must have 3 sides with a positive definite length");
-    }
-    else if (a + b > c && a + c > b && b + c > a) {
-        if (a == b && b == c) {
-            console.log("Equilateral triangle");
+    alert('Input values should be obly numbers');
+} else if (a <= 0 || b <= 0 || c <= 0) {
+      console.log('Triangle doesn\'t exist');
+      alert('A triangle must have 3 sides with a positive definite length');
+    } else if (a + b > c && a + c > b && b + c > a) {
+        if (a === b && b === c) {
+            console.log('Equilateral triangle');
+        } else if (a === b || b === c || a === c) {
+            console.log('Isosceles triangle');
+        } else {
+            console.log('Scalene triangle');
         }
-        else if (a == b || b == c || a == c) {
-            console.log("Isosceles triangle");
-        }
-        else {
-            console.log("Scalene triangle");
-        }
-    }
-    else {
-        console.log("Triangle doesn\'t exist");
-        alert("Triangle doesn\'t exist");   
+    } else {
+        console.log('Triangle doesn\'t exist');
+        alert('Triangle doesn\'t exist');   
 }
