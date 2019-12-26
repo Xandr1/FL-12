2ec3aad2686101f951fe380b5d1e10d0f08ecaa9
Oleksandr Kirsanov <olek.kirsanov@gmail.com>
2019-12-26 20:38:15 +0200

    Update after feedback

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,16 +2,19 @@ const a = parseFloat(prompt('Input value a:'));
 const b = parseFloat(prompt('Input value b:'));
 const c = parseFloat(prompt('Input value c:'));
 
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
+if (isNaN(a) || isNaN(b) || isNaN(c) || a === 0 && b === 0) {
     console.log('Invalid input data');
+} else if (a === 0) {
+    let x = c/b;
+    console.log('x = '+Math.round(x));
 } else {
     const discriminant = Math.pow(b, 2) - 4*a*c;
     if (discriminant > 0) {
-        const x1 = (-b + Math.sqrt(discriminant))/(2*a);
-        const x2 = (-b - Math.sqrt(discriminant))/(2*a);
+        let x1 = (-b + Math.sqrt(discriminant))/(2*a);
+        let x2 = (-b - Math.sqrt(discriminant))/(2*a);
         console.log('x1 = '+Math.round(x1)+' and x2 = '+Math.round(x2));
     } else if (discriminant === 0) {
-        const x = -b/(2*a);
+        let x = -b/(2*a);
         console.log('x = '+Math.round(x));
     } else {
         console.log('No solution');
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -5,7 +5,6 @@ const c = parseInt(prompt('Input length of side c:'));
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Input values should be only numbers');
 } else if (a <= 0 || b <= 0 || c <= 0) {
-    console.log('Triangle doesn\'t exist');
     alert('A triangle must have 3 sides with a positive definite length');
 } else if (a + b > c && a + c > b && b + c > a) {
     if (a === b && b === c) {
@@ -16,6 +15,5 @@ if (isNaN(a) || isNaN(b) || isNaN(c)) {
         console.log('Scalene triangle');
     }
 } else {
-    console.log('Triangle doesn\'t exist');
     alert('Triangle doesn\'t exist');   
 }
